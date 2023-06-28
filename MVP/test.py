83362fa72bb1f784019f93b9ce9ce605207cd7a1
RamblerGoodness <and17091@byui.edu>
2023-06-28 13:33:40 -0600

    "Working" prototype

--- MVP/test.py
+++ MVP/test.py
@@ -1,31 +1,30 @@
 import openai
 import config
-openai.api_key = config.api_key()
 
-
-def generate_prompt( input_prompt):
-
-    prompt = (f"Turn this into my daily schedule for one day 'prompt', "
-              f"remember that things i tell you are set, you do not modify their time, "
-              f"but fill the gaps in the schedule to be productive"
-              f"ask questions about the users day so you know what they already have planned"
-              f"build a plan then clarify with the user if it is correct and update if not"
-              f"if the user is finished then end the conversation and print the schedule in a readable format"
-              f"if the user wants to change something then ask what they want to change and update the schedule"
-              f"start by asking the user what time they want wake up and what time they go to sleep"
-              f"recommend about 8 hours of sleep"
-              f"do not take the role of the user and make decisions for them, wait for their input")
-    prompt += input_prompt
+def generate_prompt(conversation):
+    openai.api_key = config.api_key()  # Replace with your actual OpenAI API key
     response = openai.Completion.create(
         engine='text-davinci-003',
-        prompt=prompt,
-        max_tokens=300
+        prompt=conversation,
+        max_tokens=50,  # Adjust as per your needs
+        temperature=0.7,  # Adjust as per your needs
     )
-    generated_text = response.choices[0].text.strip()
-    prompt += response.choices[0].text.strip()
+    choices = response.choices[0].text.strip()
+    if choices:
+        return choices
+    else:
+        return "No response received from the API."
 
-    return response.choices[0].text.strip()
+# Set the initial context or message
+initial_message = 'You are a chat bot used to plan out the users day. The user will input a time and a task and you will add it to the schedule.'
 
+# Create the conversation history with the initial message
+conversation = f'{initial_message}\n'
+
+# Prompt the user for input and generate a response
 while True:
-
-    print(generate_prompt( input("Enter a prompt: ")))
+    user_input = input("Enter a prompt: ")
+    conversation += f'User: {user_input}\n'
+    response = generate_prompt(conversation)
+    conversation += f'system: {response}\n'
+    print(response)
